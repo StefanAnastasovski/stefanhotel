a5893cf0c9d8ca96e266f9700630eeed76a50a3b
Stefan Anastasovski <52094853+PajkoStefan@users.noreply.github.com>
2020-05-13 01:21:41 +0200

    Frontend 1

---
+++ hotel-frontend/build/precache-manifest.7b94c669c5ee32c55cdda2a8d7f52ff0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7323a3d14d9ad7f2e2097bc17651f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40d896dcafc870bad16a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "fbab4697a9c81c776ea9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "40d896dcafc870bad16a",
+    "url": "/static/js/2.aa258866.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.aa258866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbab4697a9c81c776ea9",
+    "url": "/static/js/main.6efc9079.chunk.js"
+  },
+  {
+    "revision": "79753322de31b2fb4c07",
+    "url": "/static/js/runtime-main.f8c72acc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
